ecfc40efd7b4dc193d14eea04543c96f371711b6
Josh Duff <me@JoshDuff.com>
2017-03-09 12:35:17 -0600

    Bump to Koa 2.1.0, drop the babel runtime

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-require('loud-rejection/register')
-
-require(`babel-polyfill`)
-require(`babel-register`)({
-	plugins: [
-		`transform-async-to-generator`,
-	]
-})
-
 require('./server/index').listen(process.env.PORT || 8889)
