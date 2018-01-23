aeaf0023d9f80d747af9ba4fce18f0b82c65f12d
Josh Duff <me@JoshDuff.com>
2018-01-23 13:04:54 -0600

    Autoformatting, error logging

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const Koa = require('koa')
-const send = require('koa-send')
-const conditionalGet = require('koa-conditional-get')
-const router = require('koa-router')()
+const Koa = require(`koa`)
+const send = require(`koa-send`)
+const conditionalGet = require(`koa-conditional-get`)
+const router = require(`koa-router`)()
 
-const PQueue = require('p-queue')
+const PQueue = require(`p-queue`)
 
-const makeDownloadingPathGetter = require('./download-on-demand')
-const preloadImages = require('./preload-images')
+const makeDownloadingPathGetter = require(`./download-on-demand`)
+const preloadImages = require(`./preload-images`)
 
 const downloadQueue = new PQueue({ concurrency: 5 })
 
@@ -23,28 +23,28 @@ const preloadStatus = preloadImages({
 	getImage: getImagePath,
 })
 
-router.get('/status', async function(context) {
+router.get(`/status`, async context => {
 	context.body = preloadStatus()
 })
 
 app.use(conditionalGet())
 
-router.get('/:sizeIdentifier(1|2)/:imageUrl(.*)', async function(context, next) {
+router.get(`/:sizeIdentifier(1|2)/:imageUrl(.*)`, async(context, next) => {
 	context.status = 200
-	context.set('ETag', etagValue)
+	context.set(`ETag`, etagValue)
 
 	const { sizeIdentifier, imageUrl } = context.params
 
 	if (context.stale) {
 		const images = await getImagePath(imageUrl)
 		const pathOnDisk = images[sizeIdentifier]
-		await send(context, pathOnDisk, { root: '/', setHeaders })
+		await send(context, pathOnDisk, { root: `/`, setHeaders })
 	}
 })
 
 function setHeaders(response) {
-	response.setHeader('Cache-Control', 'public')
-	response.setHeader('Last-Modified', lastModifiedValue)
+	response.setHeader(`Cache-Control`, `public`)
+	response.setHeader(`Last-Modified`, lastModifiedValue)
 }
 
 app.use(router.routes())
